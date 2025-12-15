8ce8b96e0ceb9e831c66333588506d510caa683a
felixpetschko <felixpetschko@gmail.com>
2025-12-15 11:57:43 +0100

    adapted layout of fFig 2

--- Fig_2.R
+++ Fig_2.R
@@ -5,12 +5,19 @@ library(grid)
 
 allresults_minor_andersson <- readRDS("./data/allresults_minor_andersson.rds")
 
+title_size <- 22
+font_size <- 18
+legend_size <- 20
+
 # B cells naive (C2L)
 p1 <- spacedeconv::plot_spatial(
   allresults_minor_andersson,
   result = "c2l_B.cells.Naive",
-  density = FALSE, smooth = FALSE,
-  title_size = 35, font_size = 25, legend_size = 30,
+  density = FALSE,
+  smooth = FALSE,
+  title_size = title_size ,
+  font_size = font_size ,
+  legend_size = legend_size ,
   title = "C2L Naïve B cells",
   nDigits = 2
 )
@@ -19,8 +26,12 @@ p1 <- spacedeconv::plot_spatial(
 p2 <- spacedeconv::plot_spatial(
   allresults_minor_andersson,
   result = "c2l_B.cells.Naive",
-  density = FALSE, smooth = FALSE, transform_scale = "sqrt",
-  title_size = 35, font_size = 25, legend_size = 30,
+  density = FALSE,
+  smooth = FALSE,
+  transform_scale = "sqrt",
+  title_size = title_size ,
+  font_size = font_size ,
+  legend_size = legend_size ,
   title = "C2L Naïve B cells (sqrt)",
   nDigits = 2
 )
@@ -29,18 +40,26 @@ p2 <- spacedeconv::plot_spatial(
 p3 <- spacedeconv::plot_spatial(
   allresults_minor_andersson,
   result = "c2l_B.cells.Naive",
-  density = FALSE, smooth = FALSE, transform_scale = "log2",
-  title_size = 35, font_size = 25, legend_size = 30,
+  density = FALSE,
+  smooth = FALSE,
+  transform_scale = "log2",
+  title_size = title_size ,
+  font_size = font_size ,
+  legend_size = legend_size ,
   title = "C2L Naïve B cells (log2)",
-  nDigits = 2, pseudocount = 0 # added pseudo_count = 0
+  nDigits = 2,
+  pseudocount = 0
 )
 
 # B cells naive smoothed
 p4 <- spacedeconv::plot_spatial(
   allresults_minor_andersson,
   result = "c2l_B.cells.Naive",
-  density = FALSE, smooth = TRUE,
-  title_size = 35, font_size = 25, legend_size = 30,
+  density = FALSE,
+  smooth = TRUE,
+  title_size = title_size ,
+  font_size = font_size ,
+  legend_size = legend_size ,
   title = "C2L Naïve B cells (smoothed)",
   nDigits = 2
 )
@@ -49,8 +68,11 @@ p4 <- spacedeconv::plot_spatial(
 p5 <- spacedeconv::plot_spatial(
   allresults_minor_andersson,
   result = "c2l_CAFs.MSC.iCAF.like",
-  density = FALSE, smooth = TRUE,
-  title_size = 35, font_size = 25, legend_size = 30,
+  density = FALSE,
+  smooth = TRUE,
+  title_size = title_size ,
+  font_size = font_size ,
+  legend_size = legend_size ,
   title = "C2L iCAF (smoothed)",
   nDigits = 2
 )
@@ -59,8 +81,11 @@ p5 <- spacedeconv::plot_spatial(
 p6 <- spacedeconv::plot_spatial(
   allresults_minor_andersson,
   result = "c2l_CAFs.myCAF.like",
-  density = FALSE, smooth = TRUE,
-  title_size = 35, font_size = 25, legend_size = 30,
+  density = FALSE,
+  smooth = TRUE,
+  title_size = title_size ,
+  font_size = font_size ,
+  legend_size = legend_size ,
   title = "C2L myCAF (smoothed)",
   nDigits = 2
 )
@@ -70,10 +95,14 @@ p7 <- spacedeconv::plot_comparison(
   allresults_minor_andersson,
   cell_type_1 = "c2l_CAFs.MSC.iCAF.like",
   cell_type_2 = "c2l_CAFs.myCAF.like",
-  density = FALSE, smooth = TRUE,
-  title_size = 35, font_size = 25, legend_size = 30,
+  density = FALSE,
+  smooth = TRUE,
+  title_size = title_size ,
+  font_size = font_size ,
+  legend_size = legend_size ,
   title = "C2L iCAF vs. myCAF (smoothed)",
-  palette = "Purple-Green", reverse_palette = TRUE,
+  palette = "Purple-Green",
+  reverse_palette = TRUE,
   shift_positive = FALSE,
   nDigits = 2
 )
@@ -82,8 +111,11 @@ p7 <- spacedeconv::plot_comparison(
 p8 <- spacedeconv::plot_spatial(
   allresults_minor_andersson,
   result = "rctd_Cancer.LumB.SC",
-  density = FALSE, smooth = TRUE,
-  title_size = 35, font_size = 25, legend_size = 30,
+  density = FALSE,
+  smooth = TRUE,
+  title_size = title_size ,
+  font_size = font_size ,
+  legend_size = legend_size ,
   title = "RCTD LumB (smoothed)",
   nDigits = 2
 )
@@ -95,7 +127,7 @@ grid_layout <- rbind(
 )
 
 # Arrange the plots in a grid
-grid_2 <- grid.arrange(
+final_plot <- grid.arrange(
   p1, p2, p3, p4,
   p5, p6, p7, p8,
   layout_matrix = grid_layout
@@ -104,8 +136,10 @@ grid_2 <- grid.arrange(
 # Save the UMAP as a high-resolution PNG
 ggplot2::ggsave(
   filename = "./export/fig_2B.png",
-  plot = grid_2,
-  width = 32, height = 17, units = "in",
+  plot = final_plot,
+  width = 20,
+  height = 10,
+  units = "in",
   dpi = 600,
   bg = "white"
 )
