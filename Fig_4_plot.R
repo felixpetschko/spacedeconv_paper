3e9033912c1a2c6608abd53817a975a75d70be54
felixpetschko <felixpetschko@gmail.com>
2026-01-20 19:20:48 +0100

    changed titles and tags

--- Fig_4_plot.R
+++ Fig_4_plot.R
@@ -1,10 +1,18 @@
 library(ggplot2)
-library(gridExtra)
+library(cowplot)
 
 plots <- readRDS("./export/fig_4_panels.rds")
 
 add_enum <- function(p, label) {
-  p + ggplot2::ggtitle(paste0(label, ") ", p$labels$title))
+  p +
+    labs(tag = paste0(label)) +
+    theme(
+      plot.title = element_text(hjust = 0.5, vjust = 1),
+      plot.title.position = "plot",
+      plot.tag = element_text(hjust = 0, vjust = 1, face = "bold", size = 24),
+      plot.tag.position = c(0, 1),
+      plot.margin = margin(6, 6, 6, 6)
+    )
 }
 
 add_enum_list <- function(plots) {
@@ -13,11 +21,11 @@ add_enum_list <- function(plots) {
 
 plots <- add_enum_list(plots)
 
-grid_layout <- rbind(c(1, 2, 3), c(4, 5, 6))
-
-final <- grid.arrange(
-  grobs = plots,
-  layout_matrix = grid_layout
+final <- plot_grid(
+  plotlist = plots,
+  ncol = 3,
+  align = "hv",
+  axis = "tblr"
 )
 
 ggplot2::ggsave(
