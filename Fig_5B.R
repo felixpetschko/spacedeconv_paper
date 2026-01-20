218a384d08168030578512e8779b0138d1cd0ee9
felixpetschko <felixpetschko@gmail.com>
2026-01-20 19:00:44 +0100

    added label tags per tile

--- Fig_5B.R
+++ Fig_5B.R
@@ -147,9 +147,14 @@ cafabs <- plot_spatial(
 )
 
 add_enum <- function(p, label) {
-    p + ggplot2::ggtitle(
-        bquote(bold(.(label)) * ")" * " " * .(p$labels$title))
-    )
+    p +
+        labs(tag = paste0(label)) +
+        theme(
+            plot.title = element_text(hjust = 0.5),
+            plot.title.position = "plot",
+            plot.tag = element_text(hjust = 0, vjust = 1, face = "bold", size = 24),
+            plot.tag.position = c(0, 1)
+        )
 }
 
 add_enum_list <- function(plots, labels) {
