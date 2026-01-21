1a8b70727f1f823e14ffd9491536c6f9df81725c
felixpetschko <felixpetschko@gmail.com>
2026-01-21 11:53:18 +0100

    added letter enumerations and centered titles

--- Fig_S2_plot.R
+++ Fig_S2_plot.R
@@ -3,6 +3,24 @@ library(gridExtra)
 
 plots <- readRDS("./export/fig_S2_panels.rds")
 
+add_enum <- function(p, label) {
+  p +
+    labs(tag = paste0(label)) +
+    theme(
+      plot.title = element_text(hjust = 0.5, vjust = 1),
+      plot.title.position = "plot",
+      plot.tag = element_text(hjust = 0, vjust = 1, face = "bold", size = 24),
+      plot.tag.position = c(0, 1),
+      plot.margin = margin(6, 6, 6, 6)
+    )
+}
+
+add_enum_list <- function(plots) {
+  Map(function(p, i) add_enum(p, LETTERS[i]), plots, seq_along(plots))
+}
+
+plots <- add_enum_list(plots)
+
 grid_layout <- rbind(
     c(1, 1, 1, 2, 2, 2),
     c(3, 3, 4, 4, 5, 5),
